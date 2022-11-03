ed1bdc32ed140fb9b091c75671bde85ccb871b7a
navneetMaantic <116359943+navneetMaantic@users.noreply.github.com>
2022-11-03 12:19:21 +0530

    code added if incorrect ruleName in file

--- src/main/java/com/maantic/automation/pages/DevStudioPage.java
+++ src/main/java/com/maantic/automation/pages/DevStudioPage.java
@@ -12,42 +12,40 @@ public class DevStudioPage extends BasePage {
     private By search_txtBox = By.xpath("(//input[contains(@name, 'SearchText')])[1]");
     private By search_icon_btn = By.xpath("//button[@title='Search'][i]");
     private By td_decision_table = By.xpath("//table[@id='bodyTbl_right']/tbody/tr/td/div/span/a[text()='Decision Table']/ancestor::tr[1]/td[1]/span");
+    private By td_decision_table_name = By.xpath("//table[@id='bodyTbl_right']/tbody/tr/td/div/span/a[text()='Decision Table']/following::tr[1]/td[1]/nobr/span/a");
 
     //private By tbl_search_results = By.xpath("//table[@id='bodyTbl_right']/tbody/tr/td/span");
     //private By td_ruleName = By.xpath("(//table[@id='bodyTbl_right'])[2]/tbody/tr/td/div[contains(text(),'PegaFS:08-06-01')]");
     //private By lbl_decisionTableID = By.xpath("//span[@title='Purpose'][contains(text(),'RelatedPartyEnforcedPairs')]");
 
-    public void enterSearchTermInSearchBox(String item){    //enter search text in searchBox
+    public void enterSearchTermInSearchBox(String item) {    //enter search text in searchBox
         CommonUtils.waitForVisibilityOfElement(search_txtBox);
-        CommonUtils.enterText(search_txtBox,item);
+        CommonUtils.enterText(search_txtBox, item);
     }
 
-    public void clickOnSearchIcon(){    //click on Search button
+    public void clickOnSearchIcon() {    //click on Search button
         CommonUtils.waitForVisibilityOfElement(search_icon_btn);
         CommonUtils.click(search_icon_btn);
     }
 
-    public void clickSearchResults(String ruleType, String ruleSetVersion){ //click on the search result after verifying ruleType & rulesetVersion
-        if(ruleType.equalsIgnoreCase("Decision_Table")) {
-            CommonUtils.waitForVisibilityOfElement(search_txtBox);
-            CommonUtils.click(td_decision_table);
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+    //click on the search result after verifying ruleName, ruleType & ruleSetVersion
+    public Boolean clickSearchResults(String ruleType, String ruleSetVersion, String ruleName) throws InterruptedException {
+        Boolean temp = false;
+        if (ruleType.equalsIgnoreCase("Decision_Table")) {
+            if (CommonUtils.isElementPresent(td_decision_table_name)) {
+                if (CommonUtils.getElementText(td_decision_table_name).equalsIgnoreCase(ruleName)) {
+                    CommonUtils.waitForVisibilityOfElement(search_txtBox);
+                    CommonUtils.click(td_decision_table);
+                    Thread.sleep(3000);
+                    getDriver().findElement(By.xpath("(//table[@id='bodyTbl_right'])[2]/tbody/tr/td/div[contains(text(),'" + ruleSetVersion + "')]")).click();
+                    temp = true;
+                }
             }
-            getDriver().findElement(By.xpath("(//table[@id='bodyTbl_right'])[2]/tbody/tr/td/div[contains(text(),'"+ruleSetVersion+"')]")).click();
+        } else if (ruleType.equalsIgnoreCase("Activity")) {
+
+        } else if (ruleType.equalsIgnoreCase("SLA")) {
+
         }
+        return temp;
     }
-
-    //    public String getDashboardPageContent(){
-//        CommonUtils.switchToIframe(iframe01_name);
-//        CommonUtils.waitForVisibilityOfElement(dashboard_txt);
-//        return CommonUtils.getElementText(dashboard_txt);
-//    }
-
-//    public String getLoggedInUserName(String attr){
-//        CommonUtils.waitForVisibilityOfElement(loggedIn_user_txt);
-//        return CommonUtils.getAttributeValueOfElement(attr,loggedIn_user_txt);
-//    }
 }
--- src/test/java/com/maantic/automation/tests/GBTDecisionTableTest.java
+++ src/test/java/com/maantic/automation/tests/GBTDecisionTableTest.java
@@ -14,14 +14,14 @@ import org.testng.annotations.Test;
 
 import java.util.Map;
 
-@Listeners({ AllureListeners.class })
+@Listeners({AllureListeners.class})
 public class GBTDecisionTableTest extends BasePage {
 
     LoginPage loginPage = new LoginPage();
     DevStudioPage homePage = new DevStudioPage();
     DecisionTablePage dtPage = new DecisionTablePage();
 
-    @Test(priority = 1, groups = {"Sanity","Regression"}, description = "Verifying the Decision Table is working properly.", dataProviderClass = ExcelDataProvider.class, dataProvider ="ExcelTestDataGBT" )
+    @Test(priority = 1, groups = {"Sanity", "Regression"}, description = "Verifying the Decision Table is working properly.", dataProviderClass = ExcelDataProvider.class, dataProvider = "ExcelTestDataGBT")
     @Severity(SeverityLevel.NORMAL)
     @Description("Test Case Description: Verify the Decision Table is working properly.")
     public void Decision_Table(Map<String, String> map) throws InterruptedException {
@@ -32,23 +32,30 @@ public class GBTDecisionTableTest extends BasePage {
         homePage.enterSearchTermInSearchBox(map.get("RuleName"));
         homePage.clickOnSearchIcon();
         Thread.sleep(3000);
-        homePage.clickSearchResults(map.get("RuleType"), map.get("RuleSetVersion"));
-        Assert.assertEquals(true, dtPage.isDecisionTableDisplayed(map.get("RuleName")));
-        dtPage.clickActionsRunOfDecisionTable();
-        //now switches to decision table window
-        String dt_output = dtPage.switchWindowDecisionTable(map.get("Param1"));
-        //writing 'actual' result in file
-        ExcelUtils.writeExcelData(dt_output, map.get("RuleType"), 17);  //'Actual' column=17
-        Thread.sleep(5000);
-        //writing 'pass/fail' in file
-        if(map.get("Expected").equalsIgnoreCase(dt_output)){
-            ExcelUtils.writeExcelData("Pass", map.get("RuleType"), 18); //'Pass/Fail' column=18
-            System.out.println("Pass");
+        //if correct ruleName present in file
+        if (homePage.clickSearchResults(map.get("RuleType"), map.get("RuleSetVersion"), map.get("RuleName"))) {
+            Assert.assertEquals(true, dtPage.isDecisionTableDisplayed(map.get("RuleName")));
+            dtPage.clickActionsRunOfDecisionTable();
+            //now switches to decision table window
+            String dt_output = dtPage.switchWindowDecisionTable(map.get("Param1"));
+            //writing 'actual' result in file
+            ExcelUtils.writeExcelData(dt_output, map.get("RuleType"), 17);  //'Actual' column=17
+            Thread.sleep(5000);
+            //writing 'pass/fail' in file
+            if (map.get("Expected").equalsIgnoreCase(dt_output)) {
+                ExcelUtils.writeExcelData("Pass", map.get("RuleType"), 18); //'Pass/Fail' column=18
+                System.out.println("Pass");
+            } else {
+                ExcelUtils.writeExcelData("Fail", map.get("RuleType"), 18); //'Pass/Fail' column=18
+                System.out.println("Fail");
+            }
         }
-        else {
-            ExcelUtils.writeExcelData("Fail", map.get("RuleType"), 18); //'Pass/Fail' column=18
+        else {//if incorrect ruleName present in file
+            ExcelUtils.writeExcelData("NA", map.get("RuleType"), 17);  //'Actual' column=17
+            ExcelUtils.writeExcelData("RuleName not found", map.get("RuleType"), 18); //'Pass/Fail' column=18
             System.out.println("Fail");
         }
+
     }
 
 }
